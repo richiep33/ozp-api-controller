0153bbaa5b691f5180cdea55be8e33e3bda27bbb
Ian Nelson <iannelson1@gmail.com>
2014-02-03 20:59:46 -0500

    Added stubs for ACL, Session, and User information managers.

---
+++ security/interface/session-manager/security-session-manager-v1.js
@@ -0,0 +1,9 @@
+/* jshint -W097 */
+/* jshint node:true */
+
+SecuritySessionManager = function() {};
+
+SecuritySessionManager.prototype.createSessionId = function() {};
+SecuritySessionManager.prototype.authenticate = function() {};
+SecuritySessionManager.prototype.removeSessionId = function() {};
+SecuritySessionManager.prototype.refreshSessionId = function(sessionId) {};
