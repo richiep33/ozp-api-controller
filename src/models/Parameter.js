58663425ae43aa4ca4994cc34f19fca9b7e91a1d
Eric Mosher <emoshmosh@gmail.com>
2014-02-27 16:48:29 -0500

    A few green specs in Parameter

--- src/models/Parameter.js
+++ src/models/Parameter.js
@@ -3,8 +3,45 @@
 
 "use strict";
 
-var Parameter = function() {
+var moment = require('moment');
 
+/**
+ * Defines a parameter for use with route.
+ *
+ * @param key      {String} parameter key
+ * @param operator {String} parameter operator
+ * @param value    {String} value of the parameter
+ */
+var Parameter = function(key, operator, value) {
+    this._key = key;
+    this._op = operator;
+    this._value = value;
+    if (Number(this._value)) { 
+        this._valueType = "number";
+    } else if (this._isEmail(this._value)){
+        this._valueType = "email";
+    } 
+};
+
+/**
+ * Get the type of this parameter.
+ *
+ * @return {String} the type of parameter
+ */
+Parameter.prototype.type = function(){
+    return this._valueType;
+};
+
+/**
+ * Determine whether a string is an email.
+ *
+ * @private
+ * @param  {String}  toTest the string to test
+ * @return {Boolean}        true if the string looks like an email, false otherwise
+ */
+Parameter.prototype._isEmail = function(toTest){
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(toTest);
 };
 
 module.exports = Parameter;
