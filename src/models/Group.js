8bcdba44c80631b19933f2fb4cf287250d294b1d
Eric Mosher <emoshmosh@gmail.com>
2014-03-05 11:47:13 -0500

    Adding documentation to Group code

--- src/models/Group.js
+++ src/models/Group.js
@@ -10,7 +10,12 @@ var defaults = {
 };
 
 /**
+ * Group
  *
+ * Defines a group of users
+ *
+ * @constructor
+ * @param {Object} groupObject    a group config object
  */
 var Group = function(groupObject) {
     // Merge defaults and any supplied parameters into a config object
@@ -22,30 +27,65 @@ var Group = function(groupObject) {
     this._users = config.users;
 };
 
+/**
+ * Get the group name
+ *
+ * @return {String}     the group name
+ */
 Group.prototype.name = function() {
     return this._name;
 };
 
+/**
+ * Get the group description
+ *
+ * @return {String}     the group description
+ */
 Group.prototype.description = function() {
     return this._description;
 };
 
+/**
+ * Set the name to the specified value
+ *
+ * @param {String} name     the new name
+ */
 Group.prototype.setName = function(name) {
     this._name = name;
 };
 
+/**
+ * Set the description to a new value
+ *
+ * @return {String} description     the new description
+ */
 Group.prototype.setDescription = function(description) {
     this._description = description;
 };
 
+/**
+ * Get the list of users assigned to this group
+ *
+ * @return {Array}     the users assigned to the group
+ */
 Group.prototype.users = function() {
     return this._users;
 };
 
+/**
+ * Add a user to this group
+ *
+ * @param {String} user    the user name to add
+ */
 Group.prototype.addUser = function(user) {
     this._users.push(user);
 };
 
+/**
+ * Remove the user from this group
+ *
+ * @param {String} user     the user to remove
+ */
 Group.prototype.removeUser = function(user) {
     // Find index to remove
     var index = this._users.indexOf(user);
